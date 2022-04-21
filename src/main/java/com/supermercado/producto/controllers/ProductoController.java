a40c6907c3af191ce24f0f13c8eeb6279a71b3c9
SCIS3-PC05 <afnavarroc@ufpso.edu.co>
2022-04-20 19:41:55 -0500

    Primer parcial procesos del negocio

--- src/main/java/com/supermercado/producto/controllers/ProductoController.java
+++ src/main/java/com/supermercado/producto/controllers/ProductoController.java
@@ -2,15 +2,13 @@ package com.supermercado.producto.controllers;
 
 import com.supermercado.producto.entity.Producto;
 import com.supermercado.producto.repository.ProductoRepository;
+import com.supermercado.producto.util.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 
@@ -18,6 +16,7 @@ public class ProductoController {
 
     @Autowired
     private ProductoRepository productoRepository;
+    private Message message = new Message();
 
     @RequestMapping(value = "api/productos/{id}", method = RequestMethod.GET)
     public ResponseEntity<Producto> getProducto(@PathVariable Long id){
@@ -41,5 +40,36 @@ public class ProductoController {
     public List<Producto> listProductos(){
 
         return productoRepository.findAll();
+    }
+
+    @RequestMapping(value = "api/productos/{id}", method = RequestMethod.PUT)
+    public ResponseEntity editProducto(@RequestBody Producto newProducto, @PathVariable Long id){
+        Map<String, String> response = new HashMap<>();
+        try {
+            Producto producto = productoRepository.findById(id).get();
+            producto.setCodigoPro(newProducto.getCodigoPro());
+            producto.setNombre(newProducto.getNombre());
+            producto.setValorProducto(newProducto.getValorProducto());
+            producto.setTipoProducto(newProducto.getTipoProducto());
+            producto.setCantidad(newProducto.getCantidad());
+            productoRepository.save(producto);
+
+            return message.viewMessage(HttpStatus.OK,"success","product edit success!!");
+        }catch (Exception e){
+            return message.viewMessage(HttpStatus.NOT_FOUND,"error","Product not found!");
+        }
+    }
+    @RequestMapping(value = "api/productos/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity deleteProducto(@PathVariable Long id){
+        Map<String, String> response = new HashMap<>();
+        try {
+            Producto producto = productoRepository.findById(id).get();
+            productoRepository.delete(producto);
+            return message.viewMessage(HttpStatus.OK,"success","Product delete success!!");
+        }catch (Exception e){
+            return message.viewMessage(HttpStatus.NOT_FOUND,"error","Product not found!");
+        }
+
+
     }
 }
